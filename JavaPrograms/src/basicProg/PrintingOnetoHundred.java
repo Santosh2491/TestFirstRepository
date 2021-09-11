aeaa9e81613f035101ad462f3e4d848c92f5b083
Santosh2491 <santosham18@yahoo.in>
2021-09-11 18:04:04 +0530

    Java programs

---
+++ JavaPrograms/src/basicProg/PrintingOnetoHundred.java
@@ -0,0 +1,16 @@
+package basicProg;
+
+public class PrintingOnetoHundred {
+
+	public static void main(String[] args) {
+		int num = 'A'/'A';
+		String str = "aaaaaaaaaa";
+		
+		for(int i=num;i<=(str.length()*str.length());i++)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
